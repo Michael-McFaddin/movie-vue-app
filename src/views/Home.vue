b49d9fb4ffb28d82ae59f286a82b36750a235049
mike <mikemcfaddin@gmail.com>
2020-02-21 09:13:41 -0500

    adds all restful routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,36 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-
+    <!-- create action -->
     <div>
       <h2>New Movie</h2>
-      <button v-on:click="createMovie">Add Movie</button>
+      Title: <input type="text" v-model="newMovieTitle"><br>
+      Year: <input type="text" v-model="newMovieYear"><br>
+      Director: <input type="text" v-model="newMovieDirector"><br>
+      Plot: <input type="text" v-model="newMoviePlot"><br>
+      English: <input type="text" v-model="newMovieEnglish"><br>
+      <button v-on:click="createMovie()">Add Movie</button>
     </div>
-
+    <!-- indes/show action -->
     <div v-for="movie in movies">
       <h2>Title: {{ movie.title }}</h2>
       <h3>Year: {{ movie.year }}</h3>
-      <h3>Director: {{ movie.director }}</h3>
-      <h4>Plot: {{ movie.plot }}</h4><br>
+      <button v-on:click="movie.showExtraInfo = !movie.showExtraInfo">More Info</button>
+      <div v-if="movie.showExtraInfo">
+        <h3>Director: {{ movie.director }}</h3>
+        <p>Plot: {{ movie.plot }}</p>
+        <div>
+          <!-- update action -->
+          <h4>Edit Movie</h4>
+          Title: <input type="text" v-model="movie.title"><br>
+          Year: <input type="text" v-model="movie.year"><br>
+          Director: <input type="text" v-model="movie.director"><br>
+          Plot: <input type="text" v-model="movie.plot"><br>
+          English: <input type="text" v-model="movie.english"><br>
+          <button v-on:click="updateMovie(movie)">Update</button>
+          <button v-on:click="destroyMovie(movie)">Delete</button>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -29,11 +48,21 @@ export default {
   data: function() {
     return {
       message: "Mike's Movie Picks!",
-      movies: []
+      movies: [],
+      newMovieTitle: "",
+      newMovieYear: null,
+      newMovieDirector: "",
+      newMoviePlot: "",
+      newMovieEnglish: true
     };
   },
+  //response = HTTP.get("/api/movies")
+  //@recipes = response.parse
   created: function() {
     axios.get("/api/movies").then(response => {
+      response.data.forEach(movie => {
+        movie.showExtraInfo = false;
+      });
       console.log(response.data);
       this.movies = response.data;
     });
@@ -41,11 +70,11 @@ export default {
   methods: {
     createMovie: function() {
       var params = {
-        title: "Elf",
-        year: 2003,
-        plot: "Buddy (Will Ferrell) was accidentally transported to the North Pole as a toddler and raised to adulthood among Santa's elves. Unable to shake the feeling that he doesn't fit in, the adult Buddy travels to New York, in full elf uniform, in search of his real father. As it happens, this is Walter Hobbs (James Caan), a cynical businessman. After a DNA test proves this, Walter reluctantly attempts to start a relationship with the childlike Buddy with increasingly chaotic results.",
-        director: "Jon Favreau",
-        english: true,
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        director: this.newMovieDirector,
+        plot: this.newMoviePlot,
+        english: this.newMovieEnglish,
       };
       axios.post("/api/movies", params)
         .then(response => {
@@ -54,6 +83,32 @@ export default {
         })
         .catch(error => {
           console.log(error.response.data.errors);
+        });
+    },
+    updateMovie: function(movie) {
+      var params = {
+        title: movie.title,
+        year: movie.year,
+        director: movie.director,
+        plot: movie.plot,
+        english: movie.english
+      };
+      axios.patch(`/api/movies/${movie.id}`, params)
+        .then(response => {
+          console.log("Success!", response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyMovie: function(movie) {
+      axios.delete(`/api/movies/${movie.id}`)
+        .then(response => {
+          console.log("Success!", response.data);
+          // find index of movie object in movie array
+          var index = this.movies.indexOf(movie);
+          // remove that index from the recipes array
+          this.movies.splice(index, 1);
         });
     }
   }
